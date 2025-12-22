cc73b97fbcd53e1fcdd552ffce400dc0651a069d
岛风 <jingfeng.xjf@alibaba-inc.com>
2025-12-22 12:51:53 +0800

    fix(layout): 修复文章布局组件的渲染问题
    - 取消注释解构 props 语句确保变量可用
    - 添加返回 Article 组件的 JSX 以正确渲染内容
    - 注释保留了外部脚本相关代码但未启用渲染功能

--- layout/post.jsx
+++ layout/post.jsx
@@ -3,7 +3,8 @@ const Article = require('./common/article');
 
 module.exports = class extends Component {
     render() {
-        // const { config, page, helper } = this.props;
+        const { config, page, helper } = this.props;
+        return <Article config={config} page={page} helper={helper} index={false} />;
         // const script = `<script src="https://readmore.openwrite.cn/js/readmore.js" type="text/javascript"></script>
         //     <script>
         //         const btw = new BTWPlugin();
